ad3a1af77def63e72af2bf474baae22c3a3ce6c2
hkjava6 <69789320+hkjava6@users.noreply.github.com>
2021-08-13 15:11:55 +0530

    Create Footer.js

---
+++ src/componant/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        < >
+            {/* <div id="footer" >
+
+            </div> */}
+            <div className="footer2">
+                <p>All Rights Reserved | Design By <a href="" style={{color:"white",}}>Hanzala Kathewadi</a> </p>
+            </div>
+            <a href="#" style={{position:"fixed",bottom:"20px",right:"20px",color:"blue",fontSize:"43px",textDecoration:"none"}} class="fa fa-arrow-up"></a>
+        </ >
+    )
+}
+
+export default Footer;
